56637ac124b4158b80833b995a83b34af2008831
Arnon-T <arnon.hts@gmail.com>
2019-08-21 20:51:45 -0300

    Arquivos novos e comentados.

--- calc-imc.js
+++
@@ -1,8 +0,0 @@
-function calcularIMC(){
-        var formulario  =   document.getElementById("formulario");
-        
-        //pega os valores de cada campo
-        var kilos       =   formulario.kilos.value;
-        var metros      =   formulario.metros.value;
-        var centimetros =   formulario.centimetros.value;
-}
